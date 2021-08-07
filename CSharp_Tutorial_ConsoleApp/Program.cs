bc80dfc9ae802431b1ae654a3d7a613f5e4aa1b2
jensz <jensz@DESKTOP-RM1QEBC>
2021-08-07 22:09:32 +0200

    Erste erfolgreiche Anwendung von parallelen Tasks

--- CSharp_Tutorial_ConsoleApp/Program.cs
+++ CSharp_Tutorial_ConsoleApp/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSharp_Tutorial_ConsoleApp
 {
     class Program
     {
+        static async void Worker1 ()
+        {
+            await Task.Run(() =>
+            {
+
+                Console.WriteLine("Worker 1 starts...");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine("Worker 1 - Step " + i.ToString() + "...");
+                }
+
+                Console.WriteLine("Worker 1 stops.");
+            });
+        }
+        static async void Worker2()
+        {
+            await Task.Run(() =>
+            {
+
+                Console.WriteLine("Worker 2 starts...");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine("Worker 2 - Step " + i.ToString() + "...");
+                }
+
+                Console.WriteLine("Worker 2 stops.");
+            });
+        }
+
+        
+
+        public static async Task DisplayTime()
+        {
+            //Task wait = Task.Delay(1000);
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    //wait.Wait();
+                    Thread.Sleep(500);
+                    Console.WriteLine(DateTime.Now.Millisecond);
+                }
+            });
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DisplayTime();
+            Worker1();
+            Worker2();
+            Console.WriteLine("Please enter something:");
+            Console.WriteLine("Entered string: " + Console.ReadLine());
         }
     }
 }
